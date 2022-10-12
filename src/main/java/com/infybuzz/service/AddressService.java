e1fc5ee7f39c5b9e3a9047b450974427d163d89d
hayk <haykk2018@gmail.com>
2022-10-12 23:17:29 +0400

    Initial commit

---
+++ src/main/java/com/infybuzz/service/AddressService.java
@@ -0,0 +1,43 @@
+package com.infybuzz.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.infybuzz.entity.Address;
+import com.infybuzz.repository.AddressRepository;
+import com.infybuzz.request.CreateAddressRequest;
+import com.infybuzz.response.AddressResponse;
+
+@Service
+public class AddressService {
+
+	Logger logger = LoggerFactory.getLogger(AddressService.class);
+
+	//@ComponentScan("com.infybuzz.repository") set this into config class and resolve "Could not autowire. No beans of 'AddressRepository' type found."
+	@Autowired
+	AddressRepository addressRepository;
+
+	public AddressResponse createAddress(CreateAddressRequest createAddressRequest) {
+		
+		Address address = new Address();
+		address.setStreet(createAddressRequest.getStreet());
+		address.setCity(createAddressRequest.getCity());
+		
+		addressRepository.save(address);
+		
+		return new AddressResponse(address);
+		
+	}
+	
+	public AddressResponse getById (long id) {
+		
+		logger.info("Inside getById " + id);
+		
+		Address address = addressRepository.findById(id).get();
+		
+		return new AddressResponse(address);
+	}
+	
+}
